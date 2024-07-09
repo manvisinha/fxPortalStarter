2c46d93caadb73fdae16cb5fa5f77f7f85728d88
Manvi Sinha <131350826+manvisinha@users.noreply.github.com>
2024-07-09 15:16:06 +0530

    approveDeposit.js

--- scripts/approveDeposit.js
+++ scripts/approveDeposit.js
@@ -1,39 +1,38 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
-const fxRootContractABI = require("../fxRootContractABI.json");
-const tokenContractJSON = require("../artifacts/contracts/MetaToken.sol/MetaToken.json");
+const { ethers } = require("hardhat");
+const { FXRootContractAbi } = require("../artifacts/FXRootContractAbi.js");
+const ABI = require("../artifacts/contracts/LineArt.sol/LineArt.json");
+require("dotenv").config();
 
-const tokenAddress = ""; // place your erc20 contract address here
-const tokenABI = tokenContractJSON.abi;
-const fxERC20RootAddress = "0x3658ccFDE5e9629b0805EB06AaCFc42416850961";
-const walletAddress = ""; // place your public address for your wallet here
 
 async function main() {
+  const networkAddress = "https://ethereum-sepolia-rpc.publicnode.com";
+  const privateKey = process.env.PRIVATE_KEY;
+  const provider = new ethers.providers.JsonRpcProvider(networkAddress);
 
-    const tokenContract = await hre.ethers.getContractAt(tokenABI, tokenAddress);
-    const fxContract = await hre.ethers.getContractAt(fxRootContractABI, fxERC20RootAddress);
+  const wallet = new ethers.Wallet(privateKey, provider);
+  const signer = wallet.connect(provider);
 
-    const approveTx = await tokenContract.approve(fxERC20RootAddress, 500);
-    await approveTx.wait();
+  const ArtNFT = await ethers.getContractFactory("LineArt", signer);
+  const LAnft = ArtNFT.attach("0x2A601ff6CcB9AB8416D09DAd012192286A0EF69D");
+  const fxRootAddress = "0x9E688939Cb5d484e401933D850207D6750852053";
+  const fxRoot = new ethers.Contract(fxRootAddress, FXRootContractAbi, signer);
 
-    console.log('Approval confirmed');
+  const token= [0, 1, 2, 3, 4];
+  const approveTx = await LAnft.setApprovalForAll(fxRootAddress, true);
+  await approveTx.wait();
+  console.log("Set Approval successfully called!");
 
-
-    const depositTx = await fxContract.deposit(tokenAddress, walletAddress, 500, "0x6556");
+  for (const tokenID of token) {
+    const depositTx = await fxRoot.deposit(LAnft.address, wallet.address, tokenID, "0x6566");
     await depositTx.wait();
-
-    console.log("Tokens deposited");
-  
   }
-  
-  // We recommend this pattern to be able to use async/await everywhere
-  // and properly handle errors.
-  main().catch((error) => {
+
+  console.log("LineArt NFTs have been transferred.");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
     console.error(error);
-    process.exitCode = 1;
+    process.exit(1);
   });
