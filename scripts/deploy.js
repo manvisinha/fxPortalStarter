7b5660d2175ba7e3d29acf9506f1c537eb3f8361
Manvi Sinha <131350826+manvisinha@users.noreply.github.com>
2024-07-09 15:16:42 +0530

    deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,14 +7,10 @@
 const hre = require("hardhat");
 
 async function main() {
-
-  const token = await hre.ethers.deployContract("MetaToken");
-
-  console.log("Token address:", await token.getAddress());
+  const cont= await hre.ethers.deployContract("LineArt");
+  console.log("LineArt's Contract Address: ", cont.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
